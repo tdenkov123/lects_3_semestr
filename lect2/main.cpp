96d5477a56208afcf82edb485564c7d82ff95f2f
tdenkov123 <tdenkov123@mail.ru>
2023-09-26 19:31:21 +0300

    lect 2 and lect 3
    Done lecture tasks 2 and 3.

--- lect2/main.cpp
+++ lect2/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+class FooString {
+    private:
+        string str;
+    public:
+        FooString(string p_str) : str(p_str) {};
+        ~FooString() {};
+        
+        string getString() {return str;}
+        int getLen() {return str.length();}
+        void print() {cout << str << endl;}
+};
+
 int main() {
-    
+    FooString mystr {"Halllllloooo"};
+    cout << mystr.getLen() << endl;
+    mystr.print();
+
     return 0;
 }
---
+++ lect3/src/reformat.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class Reformat {
+    private:
+    string filename_old, filename_new;
+    ifstream file_old;
+    ofstream file_new;
+
+    public:
+    Reformat(string filename_old, string filename_new) {
+        file_old.open("../files/" + filename_old);
+        file_new.open("../files/" + filename_new);
+    }
+
+    int txt_to_gift() {
+        bool bracket_open = false;
+        string str = "";
+        while (std::getline(file_old, str)){
+            if (str.find(". ") == 1 && str.find("?") != string::npos) {
+                if (bracket_open) {
+                    file_new << "\n}\n" << str << " {";
+                }
+                else {
+                    file_new << str << " {";
+                    bracket_open = true;
+                }
+            }
+            else if (str[0] == '-') {
+                file_new << "\n" << str.replace(0, 1, "~");
+            }
+            else if (str[0] == '+') {
+                file_new << "\n" << str.replace(0, 1, "=");
+            }
+        }
+    file_new << "\n}";
+    }
+};
+
+int main() {
+    Reformat ref{"file.txt", "file.gift"};
+    ref.txt_to_gift();
+    return 0;
+}
